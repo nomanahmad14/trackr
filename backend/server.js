9eec0104b92bb42228e28895b65cbe2080d0cbfb
Noman Ahmad <noman22221@gmail.com>
2026-04-06 03:57:02 +0530

    Initialize Trackr backend setup and MongoDB connection

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./config/mongodb.js";
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Connect Database
+connectDB();
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+// Root Route
+app.get("/", (req, res) => {
+  res.send("Trackr API working");
+});
+
+// Start Server
+app.listen(port, () => {
+  console.log(`Server started at port: ${port}`);
+});
